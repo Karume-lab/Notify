16f478cbc041bfc45506f3040f1241f6bd2f1122
Karume-lab <danielkarume@outlook.com>
2024-06-09 13:46:13 +0300

    remove white text

--- core/forms.py
+++ core/forms.py
@@ -3,7 +3,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field, Submit
 from . import models
 
-FORM_FIELD_STYLES = "bg-gray-50 border p-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+FORM_FIELD_STYLES = "bg-gray-50 border p-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
 
 
 class Contact(forms.ModelForm):
